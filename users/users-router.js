79d5992796e5f5038d4687f9028fca20c0db88f6
Anthony Venturini <anthonydventurini@gmail.com>
2019-04-04 12:27:36 -0700

    Adds Users Router

---
+++ users/users-router.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const db = require('../database/dbConfig')
+
+const router = express.Router();
+
+router.get("/users", async (req, res) => {
+    try{
+        const users = await db("users");
+        res.status(200).json({users})
+    }
+    catch(error){
+        res.status(500).json({message: "Error retrieving users", error})
+    }
+})
+
+module.exports = router
