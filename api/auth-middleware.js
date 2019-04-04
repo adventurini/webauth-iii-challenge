8739cbc96a31a9e18dc1524467927c6bdf768e61
Anthony Venturini <anthonydventurini@gmail.com>
2019-04-04 12:35:44 -0700

    adds jwt token file in auth middleware

---
+++ api/auth-middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if(!token){
+        res.status(403).json({msg: "Unauthorized"})
+    }
+
+
+    jwt.verify(token, 'qwertyuiop', (err, newToken) => {
+        if(err){
+            res.status(500).json({msg: "Nope"})
+        }
+
+        req.decodedToken = newToken;
+        next();
+    });
+}
